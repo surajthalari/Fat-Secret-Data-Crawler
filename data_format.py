44715a675800f17ec01f1706a72d4ef7c5cf233d
surajthalari <31706612+surajthalari@users.noreply.github.com>
2021-06-23 17:31:29 +0530

    Update data_format.py

--- data_format.py
+++ data_format.py
@@ -13,6 +13,7 @@ nutrientDetails =pd.DataFrame(columns=['foodId','servingId','metricServingAmount
 'Folate (value)', 'Folate (unit)', 'Folic acid (value)', 'Folic acid (unit)', 'Vitamin B12 (value)', 'Vitamin B12 (unit)', 'Vitamin D (value)', 'Vitamin D (unit)', 'Vitamin E (value)', 'Vitamin E (unit)',
 'Vitamin K (value)', 'Vitamin K (unit)', 'Water (value)', 'Water (unit)'])
 
+i=0
 for index, row in df.iterrows():
         each_item = json.loads(row.to_json())
         foodDetails.loc[i] = [each_item.get('food_id', None), each_item.get('food_name', None), each_item.get('food_type', None), each_item.get('brand_name', None)] 
@@ -23,6 +24,7 @@ for index, row in df.iterrows():
         each_item.get('thiamin_b1', None), 'mg', each_item.get('riboflavin_b2', None), 'mg', each_item.get('niacin_b3', None), 'mg', each_item.get('vitamin_b6', None), 'mg', each_item.get('folate_equivalent', None), 'mg',
         each_item.get('folate', None), 'mg', each_item.get('folic_acid', None), 'mg', each_item.get('vitamin_b12', None), 'mg', each_item.get('vitamin_d', None), 'mg', each_item.get('vitamin_e', None), 'mg',
         each_item.get('vitamin_k', None), 'mg', each_item.get('water', None), 'g']
+        i=i+1
     
 foodDetails.to_excel('food_details.xlsx')
 nutrientDetails.to_excel('nutrientDetails.xlsx')
